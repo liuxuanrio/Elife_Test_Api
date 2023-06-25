c6cb4e724fb59f93baaf2eb26f20649467ef1036
july <758102395@qq.com>
2023-06-25 15:20:56 +0800

    新增gmail get请求

--- elife_api_interface/ctrip_test_api/ctripday.py
+++ elife_api_interface/ctrip_test_api/ctripday.py
@@ -13,8 +13,13 @@ from datetime import datetime
 import pytz
 import requests
 
-def get_ctrip_secrect_key():
-    return 'Xa0TTStx'
+def get_ctrip_secrect_key(env):
+    if env == 'prod':
+        return 'Xa0TTStx'
+    elif env == 'dev':
+        return '12345678'
+    else:
+        return 'null'
 
 def get_ctrip_vendor_id():
     return '1000777'
@@ -54,21 +59,21 @@ class CDes:
         return decryptData.decode()
 
 
-def ctripMd5Str(timestamp, bodyLenth):
+def ctripMd5Str(timestamp, bodyLenth,env):
     """
     请求ctrip签名
     """
     # print(timestamp, bodyLenth)
-    string = get_ctrip_vendor_id() + '3.0' + str(timestamp) + get_ctrip_secrect_key() + str(bodyLenth)
+    string = get_ctrip_vendor_id() + '3.0' + str(timestamp) + get_ctrip_secrect_key(env) + str(bodyLenth)
     # string = string.lower()
     res = hashlib.md5(string.encode('utf-8')).hexdigest()
     return res
 
-def ctripMd5(version, channel, timestamp, bodyLenth, signStr):
+def ctripMd5(version, channel, timestamp, bodyLenth, signStr,env):
     """
     ctrip 签名验证
     """
-    string = get_ctrip_vendor_id() + str(version) + str(channel) + str(timestamp) + get_ctrip_secrect_key() + str(bodyLenth)
+    string = get_ctrip_vendor_id() + str(version) + str(channel) + str(timestamp) + get_ctrip_secrect_key(env) + str(bodyLenth)
     res = hashlib.md5(string.encode('utf-8')).hexdigest()
     print("需签名串：%s, 签名后：%s, 签名串：%s" % (string, res, signStr))
     return res == signStr
@@ -77,13 +82,13 @@ def ctripMd5(version, channel, timestamp, bodyLenth, signStr):
 # url = f'https://60pfokvaff.execute-api.us-east-2.amazonaws.com/dev/day/createorder/3.0/{timeStamp}/{signStr}'
 # url = f'https://60pfokvaff.execute-api.us-east-2.amazonaws.com/dev/day/cancelorder/3.0/{timeStamp}/{signStr}'
 # url = f'https://60pfokvaff.execute-api.us-east-2.amazonaws.com/dev/day/updateorder/3.0/{timeStamp}/{signStr}'
-def ctrip_day_request(data,urlstr):
+def ctrip_day_request(data,urlstr,env):
     try:
-        dec = CDes(get_ctrip_secrect_key())
+        dec = CDes(get_ctrip_secrect_key(env))
         enData = dec.encrypt(data)
         timeStamp = get_shanghai_time()
         url_info = get_ctrip_url_info()
-        signStr = ctripMd5Str(timeStamp, len(enData))
+        signStr = ctripMd5Str(timeStamp, len(enData),env)
         url = urlstr.format(timeStamp, signStr)
         # url = f'https://60pfokvaff.execute-api.us-east-2.amazonaws.com/dev/day/createorder/3.0/{}/{}'
         response = requests.post(url, data=enData)
@@ -100,4 +105,5 @@ def ctrip_day_request(data,urlstr):
 if __name__ == '__main__':
     data={"ctripOrderId":"20230612164804","vehicleTypeId":121,"duration":5,"packageCode":"6-100","orderPrice":3101,"priceMark":"a8c0d389657841a98496a2612b08f4d6-v21","auditQuantity":3,"bagQuantity":4,"passenger":{"contactName":"julytest","contactPhone":"18123425634","email":"july@ttesst.dev","contactWeChat":"weixin123432"},"routeList":[{"useTime":"2025-04-30 11:30:00","departureLocation":{"name":"虹桥国际机场","latitude":31.166056,"longitude":121.319785,"cityId":2,"cityName":"上海","detailAddress":"虹桥国际机场"},"arriveLocationList":[{"name":"上海博物馆","latitude":31.128291,"longitude":121.43558,"cityId":2,"cityName":"上海","detailAddress":"黄浦区 人民大道201号"}]},{"useTime":"2025-05-01 04:10:00","departureLocation":{"name":"上海和平飯店","latitude":31.2390388,"longitude":121.4869678,"cityId":2,"cityName":"上海","detailAddress":"中國上海市黄浦区外滩南京东路20号 邮政编码: 200002"},"arriveLocationList":[{"name":"豫園","latitude":31.2253491,"longitude":121.4774603,"cityId":2,"cityName":"上海","detailAddress":"中國上海市黃浦區豫园"}]},{"useTime":"2024-05-02 15:10:00","departureLocation":{"name":"上海和平飯店","latitude":31.2390388,"longitude":121.4869678,"cityId":2,"cityName":"上海","detailAddress":"中國上海市黄浦区外滩南京东路20号 邮政编码: 200002"},"arriveLocationList":[{"name":"上海中山公園","latitude":31.2257219,"longitude":121.4239659,"cityId":2,"cityName":"上海","detailAddress":"中國上海市长宁区长宁路780号 邮政编码: 200050"}]},{"useTime":"2025-05-04 14:10:00","departureLocation":{"name":"上海和平飯店","latitude":31.2390388,"longitude":121.4869678,"cityId":2,"cityName":"上海","detailAddress":"中國上海市黄浦区外滩南京东路20号 邮政编码: 200002"},"arriveLocationList":[{"name":"上海站","latitude":31.24967045,"longitude":121.45561058,"cityId":2,"cityName":"上海","detailAddress":"静安区 秣陵路303号"}]}],"addServiceList":[],"extJson":"{\"remark\":\"推荐游玩行程 \\u003cbr/\\u003e北京周边包车两日游 (方案1)\\u003cbr/\\u003e1: 北京出发 司机按照预约时间到达约定地点，接您出发\\u003cbr/\\u003e2: 前往古北水镇 行驶约150公里2.5小时，到达古北水镇\\u003cbr/\\u003e3: 入住古北水镇 游览古北水镇，欣赏水镇夜景，入住酒店以便第二天行程\\u003cbr/\\u003e玩法二 (方案2)\\u003cbr/\\u003e1: 宽窄箱子 行驶约14公里2小时，建议游玩3小时\\u003cbr/\\u003e推荐美食 \\u003cbr/\\u003e (方案1)\\u003cbr/\\u003e1: 一品火锅 最好的火锅，没有之一\\u003cbr/\\u003e\"}"}
     urlstr = 'https://60pfokvaff.execute-api.us-east-2.amazonaws.com/dev/day/createorder/3.0/{}/{}'
-    print(ctrip_day_request(data,urlstr))
+    env = 'dev'
+    print(ctrip_day_request(data,urlstr,env))
--- elife_api_interface/ctrip_test_api/getsign.py
+++ elife_api_interface/ctrip_test_api/getsign.py
@@ -12,8 +12,13 @@ from datetime import datetime
 import pytz
 import requests
 
-def get_ctrip_secrect_key():
-    return 'Xa0TTStx'
+def get_ctrip_secrect_key(env):
+    if env == 'prod':
+        return 'Xa0TTStx'
+    elif env == 'dev':
+        return '12345678'
+    else:
+        return 'null'
 
 def get_ctrip_vendor_id():
     return '1000777'
@@ -58,38 +63,38 @@ def day_ctripMd5Str(timestamp, bodyLenth):
     请求ctrip签名
     """
     print(timestamp, bodyLenth)
-    string = get_ctrip_vendor_id() + '3.0' + str(timestamp) + get_ctrip_secrect_key() + str(bodyLenth)
+    string = get_ctrip_vendor_id() + '3.0' + str(timestamp) + get_ctrip_secrect_key(env) + str(bodyLenth)
     # string = string.lower()
     res = hashlib.md5(string.encode('utf-8')).hexdigest()
     return res
 
 
 
-def ctripMd5Str(timestamp, bodyLenth):
+def ctripMd5Str(timestamp, bodyLenth,env):
     """
     请求ctrip签名
     """
     print(timestamp, bodyLenth)
-    string = get_ctrip_vendor_id() + '2.0' + 'JNT' + str(timestamp) + get_ctrip_secrect_key() + str(bodyLenth)
+    string = get_ctrip_vendor_id() + '2.0' + 'JNT' + str(timestamp) + get_ctrip_secrect_key(env) + str(bodyLenth)
     res = hashlib.md5(string.encode('utf-8')).hexdigest()
     return res
 
-def ctripMd5(version, channel, timestamp, bodyLenth, signStr):
+def ctripMd5(version, channel, timestamp, bodyLenth, signStr,env):
     """
     ctrip 签名验证
     """
-    string = get_ctrip_vendor_id() + str(version) + str(channel) + str(timestamp) + get_ctrip_secrect_key() + str(bodyLenth)
+    string = get_ctrip_vendor_id() + str(version) + str(channel) + str(timestamp) + get_ctrip_secrect_key(env) + str(bodyLenth)
     res = hashlib.md5(string.encode('utf-8')).hexdigest()
     print("需签名串：%s, 签名后：%s, 签名串：%s" % (string, res, signStr))
     return res == signStr
 
-def ctrip_request(data,urlstr):
+def ctrip_request(data,urlstr,env):
     try:
-        dec = CDes(get_ctrip_secrect_key())
+        dec = CDes(get_ctrip_secrect_key(env))
         enData = dec.encrypt(data)
         timeStamp = get_shanghai_time()
         url_info = get_ctrip_url_info()
-        signStr = ctripMd5Str(timeStamp, len(enData))
+        signStr = ctripMd5Str(timeStamp, len(enData),env)
         url = urlstr.format(timeStamp, signStr)
         print(signStr)
         print(enData)
@@ -108,6 +113,7 @@ def ctrip_request(data,urlstr):
 
 if __name__ == '__main__':
     data = {"ctripPurchaseOrderId": 4298304485603463, "categoryCode": "airport-dropoff", "totalPrice": 27.16, "priceMark": "ff4eece9366a4fb58ec6170bcc913a08", "fixedLocation": {"fixedCode": "SHA", "cityId": "2", "cityName": "上海"}, "vehicleType": 119, "useTime": "2023-06-15 10:06", "duseLocation": {"address": "上海市市场监督管理局", "detailAddress": "徐汇区 大木桥路1号工商大厦", "longitude": 121.461368, "latitude": 31.202388, "geoType": "GCJ02", "cityId": 2, "cityName": "上海"}, "auseLocation": {"address": "虹桥国际机场", "detailAddress": "虹桥国际机场", "longitude": 121.339785, "latitude": 31.196056, "geoType": "GCJ02", "cityId": 2, "cityName": "上海"}, "flightInfo": {}, "passenger": {"name": "送机julytest", "localPhoneAreaCode": "+86", "localPhone": "13932375023", "maskRealPhone": "153***36263", "email": "abctest@123.com", "intlPhoneAreaCode":66,"intlPhone": "19123423222"}, "masterOrderId": 4298304485603459,  "adults": 0, "children": 0, "luggage": 0, "fromType": 1}
+    env = 'dev'
     urlstr = 'https://93praqg7h9.execute-api.ap-east-1.amazonaws.com/dev/JNT/ordercreate/2.0/{}/{}'
-    print(ctrip_request(data,urlstr))
+    print(ctrip_request(data,urlstr,env))
 
